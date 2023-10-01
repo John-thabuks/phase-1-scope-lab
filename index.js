b750d4b2ec976b6b229adab7d1376153e9af7301
John-thabuks <thabuksjohn@gmail.com>
2023-10-01 17:15:48 +0300

    Assignment completed.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    if (typeof window.customerName === 'string') {
+        window.customerName = window.customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bob";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "John";
+}
